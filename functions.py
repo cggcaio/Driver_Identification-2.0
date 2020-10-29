7eaa8074512c7ab51ccc66a99aadeda4bdb1cdb0
Caio Gonçalves <cggcaio2014@gmail.com>
2020-10-29 18:12:48 -0300

    OCSVM and IF

---
+++ functions.py
@@ -0,0 +1,150 @@
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn.svm import OneClassSVM
+from sklearn.ensemble import IsolationForest
+from scipy.spatial import distance
+
+# Receives the DB and normalizes the data by column
+def normalize(original_data):
+  data = original_data.drop(columns=['Time(s)', 'Class', 'PathOrder'])
+  scaler = preprocessing.MinMaxScaler()
+  std_values = scaler.fit_transform(data)
+  data_std = pd.DataFrame(data=std_values, columns=data.columns)
+  df2 = original_data[['Time(s)','Class', 'PathOrder']].copy()
+  data_normalized = data_std.join(df2)
+  return data_normalized
+
+# Receives DataFrame normalized, the drivers selected, the window size and the selected features
+def build_df_final(data_normalized, drivers, block_sizes, selected_features):
+
+  def create_column_names(block_size,selected_features):
+    print(block_size)
+    c_names = []
+    for t in np.arange(block_size):
+      for a in selected_features:
+        c_names.append(a + '_s' + str(t))
+
+    return c_names
+  
+  for driver in drivers:
+    for b in (block_sizes):
+      # Generate column names
+      c_names = create_column_names(b,selected_features)
+
+      # Create a data frame for driver and block_size = b
+      data = pd.DataFrame(columns=c_names)
+
+      # Select driver
+      driver_df = data_normalized[data_normalized["Class"] == driver] 
+      
+      # Sweep driver records (1 record per second)
+      for i in tqdm(np.arange(len(driver_df)-b)):
+        row = []
+        time_stamps = []
+
+        for j in np.arange(b):
+          # Get register i+j
+          df_temp = driver_df.iloc[i+j]
+          
+          # Build row with selected features
+          row = row + [df_temp[a] for a in selected_features]
+          time_stamps.append(df_temp['Time(s)'])
+
+        # Check time consistency
+        df_temp = driver_df.iloc[i]
+        if len(set(np.arange(df_temp['Time(s)'],df_temp['Time(s)']+b)).intersection(time_stamps) ) == len(time_stamps):
+          # Add rows to dataframe if times are consistent
+          #row = row +
+          row_df = pd.DataFrame(np.array([row]),columns=c_names)
+          data = pd.concat([data,row_df])
+
+      #data.to_csv('driver_' + driver +'_block_'+ str(b) + 's')
+  return data
+
+def split_data(data_std):
+  labels = np.ones((data_std.shape[0],1))
+  x_train, x_val, y_train, y_val = train_test_split(data_std,  labels, test_size=0.2, random_state=42)
+  return x_train, x_val
+
+def clusters_of_maneuvers(x_train):
+  x_train = pd.DataFrame(x_train)
+  kmeans = KMeans(n_clusters=10, random_state=42)
+  kmeans.fit(x_train)
+  x_train['K-Class'] = kmeans.labels_
+  labels_train = kmeans.labels_
+  centroid_train = kmeans.cluster_centers_
+  return labels_train, centroid_train, x_train
+
+################## ISOLATION FOREST #####################
+def train_model_if( labels_train, centroid_train, x_train_class, x_val):
+  if_list = []
+  for n in range(0, max(labels_train)+1):
+    df_cluster = x_train_class[x_train_class['K-Class'] == n]
+    model = IsolationForest(n_estimators=6, random_state=42).fit(x_train_class.drop(columns=['K-Class']))
+    if_list.append({'cluster': n, 'driver': 'a', 'model': model, 'centroid': centroid_train[n], 'x_val': x_val })
+  return if_list
+def test_model_if(if_list, data_final, data_impostor, centroides):
+  result = []
+  
+  for i in range(len(data_impostor)):   # Percorrer o DF de um motorista impostor
+    menor = 1000
+    for m in range(len(centroides)): # Percorrer entre as 10 manobras 
+      dist = distance.euclidean(data_impostor.iloc[i], centroides[m]) # Calcular se uma manabora do motorista impostor se aproxima desse cluster N do motorista principal
+      #print('Distancia ', m , 'valor ', dist)
+      if (dist<menor):
+        menor = dist
+        manobra_correspondente = m
+    result.append(if_list[manobra_correspondente]['model'].predict([data_impostor.iloc[i].values])[0])
+  return result
+#########################################################
+
+
+################## ONE CLASS SVM ########################
+def train_model_ocsvm( labels_train, centroid_train, x_train_class, x_val):
+  ocsvm_list = []
+  for n in range(0, max(labels_train)+1):
+    df_cluster = x_train_class[x_train_class['K-Class'] == n]  
+    model = OneClassSVM(kernel='sigmoid', nu=0.2).fit(x_train_class.drop(columns=['K-Class']))
+    ocsvm_list.append({'cluster': n, 'driver': 'a', 'model': model, 'centroid': centroid_train[n], 'x_val': x_val })
+  return ocsvm_list
+def test_model_ocsvm(oscvm_list, data_final, data_impostor, centroides):
+  result = []
+  #data_impostor = x_val
+  
+  for i in range(len(data_impostor)):   # Percorrer o DF de um motorista impostor
+    menor = 1000
+    for m in range(len(centroides)): # Percorrer entre as 10 manobras 
+      dist = distance.euclidean(data_impostor.iloc[i], centroides[m]) # Calcular se uma manabora do motorista impostor se aproxima desse cluster N do motorista principal
+      #print('Distancia ', m , 'valor ', dist)
+      if (dist<menor):
+        menor = dist
+        manobra_correspondente = m
+    result.append(oscvm_list[manobra_correspondente]['model'].predict([data_impostor.iloc[i].values])[0])
+  return result
+#########################################################
+
+def evaluating_result(result):
+  score = 0
+  quantidade_manobras = 0
+  contador_positivos = 0
+  for r in range(len(result)):
+    if (result[r]==1):
+      score = score + 1
+      contador_positivos = contador_positivos + 1 
+    
+    if (result[r]==-1):
+      if (contador_positivos>quantidade_manobras):
+        quantidade_manobras = contador_positivos
+        contador_positivos = 0  
+
+
+  acc = (score / len(result))*100 # Acurácia - Porcentagem de manobras que foram classificadas como normais
+
+  return acc, quantidade_manobras
+
+if __name__ == '__main__':
+  print("")
---
+++ main.py
@@ -0,0 +1,68 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+from scipy.spatial import distance
+import numpy as np
+from sklearn import preprocessing
+from tqdm import tqdm
+
+# Local Functions
+from functions import normalize
+from functions import build_df_final
+#from functions import get_maneuvers
+from functions import train_model_ocsvm
+from functions import test_model_ocsvm
+from functions import evaluating_result
+from functions import train_model_if
+from functions import test_model_if
+from functions import split_data
+from functions import clusters_of_maneuvers
+
+# Utilizar somente quando quiser normalizar os dados novamente
+#print('Normalizing data')
+#original_data = pd.read_csv("https://raw.githubusercontent.com/cggcaio/Anomaly-Detection-for-Driver-Identification/master/Data_Bases/KIA_DB/Driving%20Data(KIA%20SOUL)_(150728-160714)_(10%20Drivers_A-J).csv")
+#data_normalized = (normalize(original_data))
+
+#data_normalized = pd.read_csv("https://raw.githubusercontent.com/cggcaio/Anomaly-Detection-for-Driver-Identification/master/Data_Bases/KIA_DB/data_normalized.csv")
+data_normalized = pd.read_csv('data_normalized.csv')
+
+# PARAMETERS
+driver = ['A']
+impostor = ['B'] 
+window_size = [ 5 ]
+selected_features = ['Intake_air_pressure','Engine_soacking_time', 'Long_Term_Fuel_Trim_Bank1', 'Torque_of_friction', 'Engine_coolant_temperature', 'Steering_wheel_speed']
+method = 'OCSVM'
+
+
+
+print("Building DF for Driver", driver, "with Window_Size", window_size)
+data_final = build_df_final(data_normalized, driver, window_size, selected_features)
+
+print("Building DF for Impostor", impostor, "with Window_Size", window_size)
+data_impostor = build_df_final(data_normalized, impostor, window_size, selected_features )
+
+print('Doing data split')
+x_train, x_val = split_data(data_final)
+
+print('Create clusters')
+labels_train, centroid_train, x_train_class = clusters_of_maneuvers(x_train)
+
+if(method=='IF'):
+  print('Training IF')
+  if_list = train_model_if(labels_train, centroid_train, x_train_class, x_val)
+
+  print('Doing predictions IF')
+  result = test_model_if(if_list, data_final, data_impostor, centroid_train)
+
+
+elif(method=='OCSVM'):
+  print('Training OCSVM')
+  ocsvm_list = train_model_ocsvm(labels_train, centroid_train, x_train_class, x_val)
+
+  print('Doing predictions OCSVM')
+  result = test_model_ocsvm(ocsvm_list, data_final, data_impostor, centroid_train)
+
+print('Evaluanting the results')
+acc, quantidade_manobras = evaluating_result(result)
+
+print('Acurácia: ', acc, '% MANOBRAS CLASSIFICADAS COMO NORMAIS')
+print('Quantidade de manobras necessárias para detectar uma anomalia: ', quantidade_manobras)
